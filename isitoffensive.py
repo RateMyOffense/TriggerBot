6bfc7b28b3ab53ee051d100727d60f127d0e2925
Andrew Morgan <anoa@openmailbox.org>
2017-02-12 04:42:22 +0000

    Replies to tweets

---
+++ isitoffensive.py
@@ -0,0 +1,56 @@
+#/usr/bin/env python
+
+# Import sys for reading url from command line
+import sys
+import random
+
+# Import AI API
+from clarifai import rest
+from clarifai.rest import ClarifaiApp
+from clarifai.rest import Image as ClImage
+
+# Get the url from the command line
+url = sys.argv[1]
+
+# DEBUG: Print out given URL
+#print('We got URL: ' + url)
+
+# If we get a \n char, remove it
+url = url.rstrip()
+
+# Create an 'app' object based off our Clarifai API Key
+# This gives us access to the API
+app = ClarifaiApp("J776KJ-Ob-GtKnS_jylW-yxWrT-gmDrHfRLzeJG-", "1eevauzVbedZnKdcuMCGoZHtzLVSaJ5ndeANzdBG")
+
+# Load in their model to distinguish NSFW images
+model = app.models.get("isoffensive")
+
+# Give it an image
+image = ClImage(url=url)
+
+# Get probability it is NSFW
+response = model.predict([image])
+
+# Get how NSFW it thinks it is and store in a variable
+nsfwAmount = response["outputs"][0]["data"]["concepts"][1]["value"]
+
+# Great quotes from jesus himself
+offended_Response = ["There are kids on here you know", "Sorry", "That's a little too much, have this instead", "No, not again :(", "Spicy"]
+not_Offended_response = ["This is not offensive", "This is okay", "Woah, that was a close one",	"I hope the AI isn't wrong"]
+
+# If the nsfw probability is less than 0.15, it is most likely SFW
+# If the nsfw probability is greater than 0.85, it is most likely NSFW
+if nsfwAmount > 0.15:
+	# Pick a random offended response
+	text_response = random.choice(offended_Response)
+
+	# Is this considered a reply? Or just a mention..
+	#api.update_status('@<username> ' + text_Response, tweetId)
+
+	# This will insert the random response into the tweet, along
+	# With the probability
+	print (text_response + ", AI found image to be: " + str(nsfwAmount) + "% offensive.")
+else:
+	# Pick a random non-offended response
+	text_response = random.choice(not_Offended_response)
+	print (text_response + ", AI found image to be: " + str(nsfwAmount) + "% offensive.")
--- twitter-test.py
+++ twitter-test.py
@@ -1,5 +1,6 @@
 # Import our Twitter library
 import tweepy
+import subprocess # For running commands
 
 # Add in a Stream class to get tweets as soon as they're posted
 class MyStreamListener(tweepy.StreamListener):
@@ -7,6 +8,7 @@ class MyStreamListener(tweepy.StreamListener):
 	# On receiving a tweet, print the image URL and tweet id
 	def on_status(self, status):
 		image_url = "User did not post an image" # Changes if they did
+		out = "You need to post an image."
 
 		# Can cause an exception if the person 
 		# does not post with an image
@@ -16,14 +18,48 @@ class MyStreamListener(tweepy.StreamListener):
 			pass # If it errors, it will use the above image_url
 
 		tweet_id = status.id
+		username = status.author._json['screen_name']
 		print('image url:' + image_url)
 		print('tweet id: ' + str(tweet_id))
+		print('username: ' + username)
 
 		# Put code for talking to our AI here
 		# python3 ./isitoffensive image_url
+		if "User did not post an image" not in image_url:
+			process = subprocess.Popen(['/usr/bin/python3', '/home/ubuntu/TwitterBot/isitoffensive.py', image_url], stdout=subprocess.PIPE)
+		# We don't need stderr, since the AI script doesn't
+		# write there
+
+		# Well we know the image URL is right, let's print
+		# Out what we're getting in the other script.
+
+		# That runs the isitoffensive scripts Greg and I made,
+		# and it will return some text to tweet
+
+		# This will grab the output and store it in a variable
+		# called out, let's see what's stored there when we 
+		# check the tweeted image
+			out = process.communicate()[0].decode("utf-8")
+
+		# Add username to output
+		out = '@' + username + ' ' + out
+
+		print('We will tweet: ' + str(out))
 
 		# Reply based on above response from the AI here
-	
+		
+		# Get a random happy image
+		hi_filepath = '/home/ubuntu/images/nonoffensive/'
+		hi_filename = 'happy.jpg'
+		happy_image = hi_filepath + hi_filename
+		# I sort of deleted all our non-offensive images
+		# To save space, so we'll need to download some more
+
+		# Send the reply
+		api.update_with_media (happy_image, status=out, in_reply_to_status_id=tweet_id)
+
+		print('Reply sent')
+
 	# If we get an error, handle it instead of crashing :)
 	def on_error(self, status_code):
 		# If we hit our rate limit cap
